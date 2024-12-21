6881e278e7d206721559a298d982547946d5c4de
David Almonacid <david.almonacidf@gmail.com>
2024-12-21 03:06:45 +0000

    Refactor Pizza and PizzaDb classes for improved formatting and readability

--- PizzaStore/Pizza.cs
+++ PizzaStore/Pizza.cs
@@ -1,9 +1,9 @@
-namespace PizzaStore.Models 
+namespace PizzaStore.Models
 {
-  public class Pizza
-  {
-      public int Id { get; set; }
-      public string? Name { get; set; }
-      public string? Description { get; set; }
-  }
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
 }
--- PizzaStore/PizzaDb.cs
+++ PizzaStore/PizzaDb.cs
@@ -1,16 +1,26 @@
-using PizzaStore.Models;
 using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 namespace PizzaStore.Data;
 
 public class PizzaDb : DbContext
 {
-    public PizzaDb(DbContextOptions options) : base(options) { }
+    public PizzaDb(DbContextOptions options)
+        : base(options) { }
+
     public DbSet<Pizza> Pizzas { get; set; } = null!;
 
-    protected override void OnModelCreating (ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Pizza>().HasData(
-            new Pizza { Id = 1, Name = "Pepperoni", Description = "Classic Pepperoni Pizza"});
+        modelBuilder
+            .Entity<Pizza>()
+            .HasData(
+                new Pizza
+                {
+                    Id = 1,
+                    Name = "Pepperoni",
+                    Description = "Classic Pepperoni Pizza",
+                }
+            );
     }
 }
